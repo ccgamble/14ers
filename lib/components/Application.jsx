c67d8cb68d56c1fd573787cbd03fd1b64b2ad8d6
ccgamble <christine.c.gamble@gmail.com>
2016-12-20 14:07:03 -0700

    data rendering

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import data from '../data.js'
 
 class Application extends Component {
   constructor() {
     super();
-  }
+		this.state = {
+			data: ''
+		}
+		}
+
 
 	render() {
+		// const dataList = data.map(d =>
+		// console.log(d.name) )
+
 		return(
-			<div>hi</div>
+			<div>
+				<ul>{data.map(d=>
+					<li key={d.rank}>{d.name}, Elevation: {d.elevation} feet, Difficulty: {d.difficulty}</li>
+				)}</ul>
+			</div>
 		)
 	}
 }
+
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ module.exports = {
       },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass' },
+			{ test: /\.json$/, loader: 'json' },
+
     ],
   },
   devtool: 'source-map',
