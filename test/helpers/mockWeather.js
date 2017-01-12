9ffb6111882728680e238b96187199426851f1e8
ccgamble <christine.c.gamble@gmail.com>
2017-01-12 04:24:13 -0700

    adds tests for weather; 33 passing, 4 pending

---
+++ test/helpers/mockWeather.js
@@ -0,0 +1,1 @@
+module.exports =[{conditions: "clear", high: 72, low: 27, day: "Sunday"}, {conditions: "clear", high: 63, low: 34, day: "Sunday"}, {conditons: "rain", high: 55, low: 22, day: "Sunday"}]
