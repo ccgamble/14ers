a3ff252591ebf16f7f3364bc1945da0d684dd6dd
ccgamble <christine.c.gamble@gmail.com>
2017-02-28 23:30:36 -0700

    Adds route source info

--- lib/components/Routes.jsx
+++ lib/components/Routes.jsx
@@ -13,6 +13,9 @@ export default class Routes extends Component {
 			<div className="routes">
 				<h4 className="individual-mountain-header">Route</h4>
 				<p className="route-info">{location.route}</p>
+        <p className="route-source">Route information from 
+          <a target="_blank" href="http://www.cmc.org/ProductDetail/tabid/237/rvdsfpid/the-colorado-14ers-a-cmc-pack-guide-3rd-edition-9/rvdsfcatid/books-2/Default.aspx"> The Colorado Mountain Club.</a>
+        </p>
 			</div>
 		)
 	}
